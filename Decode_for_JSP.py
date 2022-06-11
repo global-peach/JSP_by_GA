968c3ff7a0a795cca0fa2c37ebac3458d0188e2c
tangtao <875034075@qq.com>
2022-06-11 23:10:26 +0800

    首次提交

---
+++ Decode_for_JSP.py
@@ -0,0 +1,135 @@
+from ast import Not
+from cgitb import reset
+import matplotlib.pyplot as plt
+from Jobs import Job
+from Machines import Machine_Time_window
+import numpy as np
+ 
+class Decode:
+    def __init__(self,J,Processing_time,M_num, Machine_start_time):
+        self.Processing_time = Processing_time
+        self.Machine_start_time = Machine_start_time
+        self.Scheduled = []  # 已经排产过的工序
+        self.M_num = M_num
+        self.Machines = []  # 存储机器类
+        self.fitness = 0
+        self.J=J            #
+        for j in range(M_num):
+            self.Machines.append(Machine_Time_window(j))
+        self.Machine_State = np.zeros(M_num, dtype=int)  # 在机器上加工的工件是哪个
+        self.Jobs = []     #存储工件类
+        for k, v in J.items():
+            self.Jobs.append(Job(k, v))
+    #时间顺序矩阵和机器顺序矩阵
+    def Order_Matrix(self,MS):
+        JM=[]
+        T=[]
+        Ms_decompose=[]
+        Site=0
+        for S_i in self.J.values():
+            Ms_decompose.append(MS[Site:Site+S_i])
+            Site+=S_i
+        for i in range(len(Ms_decompose)):
+            JM_i=[]
+            T_i=[]
+            for j in range(len(Ms_decompose[i])):
+                O_j=self.Processing_time[i][j]
+                M_ij=[]
+                T_ij=[]
+                for Mac_num in range(len(O_j)):  # 寻找MS对应部分的机器时间和机器顺序
+                    if O_j[Mac_num] != 9999:
+                        M_ij.append(Mac_num)
+                        T_ij.append(O_j[Mac_num])
+                    else:
+                        continue
+                JM_i.append(M_ij[Ms_decompose[i][j]])
+                T_i.append(T_ij[Ms_decompose[i][j]])
+            JM.append(JM_i)
+            T.append(T_i)
+        return JM,T
+    def Earliest_Start(self,Job,O_num,Machine,JM):
+        P_t=self.Processing_time[Job][O_num][Machine]
+        last_O_end = self.Jobs[Job].Last_Processing_end_time  # 上道工序结束时间
+        Selected_Machine=Machine
+        machine_start = 0   # 机器开工时间
+        if (self.Machine_start_time is not None and len(self.Machine_start_time) > Selected_Machine):
+            machine_start = self.Machine_start_time[Selected_Machine]
+        M_window = self.Machines[Selected_Machine].Empty_time_window()
+        M_Tstart = M_window[0]
+        M_Tend = M_window[1]
+        M_Tlen = M_window[2]
+        Machine_end_time = max(self.Machines[Selected_Machine].End_time, machine_start)
+        #流转需求:本机器上加工的工件要流转到下一个机器，本机器才能开始加工下一个工件， 与原作的时间窗不同
+        Next_Machine_start_time = self.Get_Job_Shift_Time(self.Machines[Selected_Machine],JM)
+        ealiest_start = max(last_O_end, Machine_end_time, Next_Machine_start_time)
+        # if M_Tlen is not None:  # 此处为全插入时窗
+        #     for le_i in range(len(M_Tlen)):
+        #         if M_Tlen[le_i] >= P_t:
+        #             if M_Tstart[le_i] >= last_O_end:
+        #                 ealiest_start=M_Tstart[le_i]
+        #                 break
+        #             if M_Tstart[le_i] < last_O_end and M_Tend[le_i] - last_O_end >= P_t:
+        #                 ealiest_start = last_O_end
+        #                 break
+        M_Ealiest = ealiest_start
+        End_work_time = M_Ealiest + P_t
+        return M_Ealiest, Selected_Machine, P_t, O_num,last_O_end,End_work_time
+    def Get_Job_Shift_Time(self, machine,JM):
+        #也就是本机器上一个工件的下一道工序所在机器的开始时间(流转时刻)
+        #获取本机器上一个工件
+        if len(machine.assigned_task) == 0:
+            #机器未加工过任何工件
+            return 0
+        else:
+            last_task = machine.assigned_task[-1]
+            last_job = self.Jobs[last_task[0] - 1]
+            last_o = last_task[1]
+            if last_job.Operation_num > last_o:
+                #还有未完成的工序
+                next_o_machine = self.Machines[JM[last_task[0] - 1][last_task[1]]]
+                #由于下一个机器也会有加工的工件，而其流转时刻又受下下个机器影响，所以需要计算
+                next_machine_time = 0
+                if len(next_o_machine.assigned_task) > 0:
+                    next_machine_time = next_o_machine.O_start[next_o_machine.assigned_task.index([last_task[0], last_task[1] + 1])]
+                return max(machine.End_time, next_machine_time)
+            else:
+                #工件所有工序加工完成，返回加工完成时间
+                return machine.End_time
+    #解码
+    def Decode_1(self,CHS,Len_Chromo):
+        MS=list(CHS[0:Len_Chromo])
+        OS=list(CHS[Len_Chromo:2*Len_Chromo])
+        Needed_Matrix=self.Order_Matrix(MS)
+        JM=Needed_Matrix[0]
+        for i in OS:
+            Job=i
+            O_num=self.Jobs[Job].Current_Processed()
+            Machine=JM[Job][O_num]
+            Para=self.Earliest_Start(Job,O_num,Machine,JM)
+            self.Jobs[Job]._Input(Para[0],Para[5],Para[1])
+            if Para[5]>self.fitness:
+                self.fitness=Para[5]
+            self.Machines[Machine]._Input(Job,Para[0],Para[2],Para[3])
+        return self.fitness
+ 
+    def Gantt(self,Machines):
+        M = ['red', 'blue', 'yellow', 'orange', 'green', 'palegoldenrod', 'purple', 'pink', 'Thistle', 'Magenta',
+             'SlateBlue', 'RoyalBlue', 'Cyan', 'Aqua', 'floralwhite', 'ghostwhite', 'goldenrod', 'mediumslateblue',
+             'navajowhite',
+             'navy', 'sandybrown', 'moccasin']
+        for i in range(len(Machines)):
+            Machine=Machines[i]
+            Start_time=Machine.O_start
+            End_time=Machine.O_end
+            for i_1 in range(len(End_time)):
+                # plt.barh(i,width=End_time[i_1]-Start_time[i_1],height=0.8,left=Start_time[i_1],\
+                #          color=M[Machine.assigned_task[i_1][0]],edgecolor='black')
+                # plt.text(x=Start_time[i_1]+0.1,y=i,s=Machine.assigned_task[i_1])
+                plt.barh(i, width=End_time[i_1] - Start_time[i_1], height=0.8, left=Start_time[i_1], \
+                         color='white', edgecolor='black')
+                plt.text(x=Start_time[i_1] + (End_time[i_1] - Start_time[i_1])/2-0.5, y=i, s=Machine.assigned_task[i_1][0])
+        plt.yticks(np.arange(i + 1), np.arange(1, i + 2))
+        plt.title('Scheduling Gantt chart')
+        plt.ylabel('Machines')
+        plt.xlabel('Time')
+        plt.show()
---
+++ Encode_for_JSP.py
@@ -0,0 +1,154 @@
+import numpy as np
+import random
+ 
+class Encode:
+    def __init__(self,Matrix,Pop_size,J,J_num,M_num, Processing_group):
+        self.Matrix=Matrix      #工件各工序对应各机器加工时间矩阵
+        self.GS_num=int(0.6*Pop_size)      #全局选择初始化
+        self.LS_num=int(0.3*Pop_size)     #局部选择初始化
+        self.RS_num=int(0.1*Pop_size)     #随机选择初始化
+        self.J=J                #各工件对应的工序数
+        self.J_num=J_num        #工件数
+        self.M_num=M_num        #机器数
+        self.Processing_group = Processing_group #工件分组
+        self.CHS=[]
+        self.Len_Chromo=0
+        for i in J.values():
+            self.Len_Chromo+=i
+ 
+    #生成工序准备的部分
+    def OS_List(self):
+        tmp = []
+        for i in range(len(self.J)):
+            tmp.append(i)
+        if (self.Processing_group is not None and len(self.Processing_group) > 0):
+            tmp_group = []
+            for group in self.Processing_group:
+                if (len(group) > 0):
+                    tmp_group.append(group)
+                    for i in group:
+                        tmp.remove(i)
+            for i in tmp:
+                tmp_group.append([i])
+            random.shuffle(tmp_group)
+            tmp.clear()
+            for group in tmp_group:
+                for i in group:
+                    tmp.append(i)
+        else:
+            random.shuffle(tmp)
+        OS_list=[]
+        for i in tmp:
+            O_num = self.J[i + 1]
+            for o in range(O_num):
+                OS_list.append(i)
+        return OS_list
+ 
+    #生成初始化矩阵
+    def CHS_Matrix(self, C_num):  # C_num:所需列数
+        return np.zeros([C_num, self.Len_Chromo], dtype=int)
+ 
+    def Site(self,Job,Operation):
+        O_num = 0
+        for i in range(len(self.J)):
+            if i == Job:
+                return O_num + Operation
+            else:
+                O_num = O_num + self.J[i + 1]
+        return O_num
+ 
+    #全局选择初始化
+    def Global_initial(self):
+        MS=self.CHS_Matrix(self.GS_num)
+        OS_list= self.OS_List() # 生成工序排序部分
+        OS=self.CHS_Matrix(self.GS_num)
+        for i in range(self.GS_num):
+            Machine_time = np.zeros(self.M_num, dtype=float)  # 机器时间初始化
+            OS[i] = np.array(OS_list)
+            GJ_list = [i_1 for i_1 in range(self.J_num)]
+            random.shuffle(GJ_list)
+            for g in GJ_list:  # 随机选择工件集的第一个工件,从工件集中剔除这个工件
+                h = self.Matrix[g]  # 第一个工件含有的工序
+                for j in range(len(h)):  # 从工件的第一个工序开始选择机器
+                    D = h[j]
+                    List_Machine_weizhi = []
+                    for k in range(len(D)):  # 每道工序可使用的机器以及机器的加工时间
+                        Useing_Machine = D[k]
+                        if Useing_Machine != 9999:  # 确定可加工该工序的机器
+                            List_Machine_weizhi.append(k)
+                    Machine_Select = []
+                    for Machine_add in List_Machine_weizhi:  # 将这道工序的可用机器时间和以前积累的机器时间相加
+                        #  比较可用机器的时间加上以前累计的机器时间的时间值，并选出时间最小
+                        Machine_Select.append(Machine_time[Machine_add] + D[
+                            Machine_add])
+                    Min_time = min(Machine_Select)
+                    K = Machine_Select.index(Min_time)
+                    I = List_Machine_weizhi[K]
+                    Machine_time[I] = Min_time
+                    site=self.Site(g,j)
+                    MS[i][site] = K
+        CHS1 = np.hstack((MS, OS))
+        return CHS1
+ 
+ 
+    #局部选择初始化
+    def Local_initial(self):
+        MS = self.CHS_Matrix(self.LS_num)
+        OS_list = self.OS_List()# 生成工序排序部分
+        OS = self.CHS_Matrix(self.LS_num)
+        for i in range(self.LS_num):
+            (OS_list)  
+            OS_gongxu = OS_list
+            OS[i] = np.array(OS_gongxu)
+            GJ_list = [i_1 for i_1 in range(self.J_num)]
+            for g in GJ_list:
+                Machine_time = np.zeros(self.M_num)  # 机器时间初始化
+                h =self.Matrix[g]   # 第一个工件及其对应工序的加工时间
+                for j in range(len(h)):  # 从工件的第一个工序开始选择机器
+                    D = h[j]
+                    List_Machine_weizhi = []
+                    for k in range(len(D)):  # 每道工序可使用的机器以及机器的加工时间
+                        Useing_Machine = D[k]
+                        if Useing_Machine == 9999:  # 确定可加工该工序的机器
+                            continue
+                        else:
+                            List_Machine_weizhi.append(k)
+                    Machine_Select = []
+                    for Machine_add in List_Machine_weizhi:  # 将这道工序的可用机器时间和以前积累的机器时间相加
+                        Machine_time[Machine_add] = Machine_time[Machine_add] + D[
+                            Machine_add]  # 比较可用机器的时间加上以前累计的机器时间的时间值，并选出时间最小
+                        Machine_Select.append(Machine_time[Machine_add])
+                    Machine_Index_add = Machine_Select.index(min(Machine_Select))
+                    site = self.Site(g, j)
+                    MS[i][site] = MS[i][site] + Machine_Index_add
+        CHS1 = np.hstack((MS, OS))
+        return CHS1
+ 
+    def Random_initial(self):
+        MS = self.CHS_Matrix(self.RS_num)
+        OS_list = self.OS_List()# 生成工序排序部分
+        OS = self.CHS_Matrix(self.RS_num)
+        for i in range(self.RS_num):
+            OS_gongxu = OS_list
+            OS[i] = np.array(OS_gongxu)
+            GJ_list = [i_1 for i_1 in range(self.J_num)]
+            A = 0
+            for gon in GJ_list:
+                Machine_time = np.zeros(self.M_num)  # 机器时间初始化
+                g = gon  # 随机选择工件集的第一个工件   #从工件集中剔除这个工件
+                h = np.array(self.Matrix[g])  # 第一个工件及其对应工序的加工时间
+                for j in range(len(h)):  # 从工件的第一个工序开始选择机器
+                    D = np.array(h[j])
+                    List_Machine_weizhi = []
+                    Site=0
+                    for k in range(len(D)):  # 每道工序可使用的机器以及机器的加工时间
+                        if D[k] == 9999:  # 确定可加工该工序的机器
+                            continue
+                        else:
+                            List_Machine_weizhi.append(Site)
+                            Site+=1
+                    Machine_Index_add = random.choice(List_Machine_weizhi)
+                    MS[i][A] = MS[i][A] + Machine_Index_add
+                    A += 1
+        CHS1 = np.hstack((MS, OS))
+        return CHS1
